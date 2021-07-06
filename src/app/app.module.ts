daea6636f55aff1144e4447752b70c5e453d3e4e
imsorx <srbhkp@gmail.com>
2021-07-06 16:01:50 +0530

    Fixed md image issue and added custom scrollbar for mozilla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { AboutComponent } from './components/about/about.component';
-import { TimelineComponent } from './components/timeline/timeline.component';
-import { ResourcesComponent } from './components/resources/resources.component';
-import { MarkdownModule } from 'ngx-markdown';
 import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoadingInterceptor } from './interceptors/loading.interceptor';
-import { ScrollToTopComponent } from './components/scroll-to-top/scroll-to-top.component';
+import { MarkdownModule } from 'ngx-markdown';
+import { AppComponent } from './app.component';
+import {
+  HeaderComponent,
+  FooterComponent,
+  ScrollToTopComponent,
+  AboutComponent,
+  TimelineComponent,
+  ResourcesComponent
+} from './components';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
---
+++ src/app/components/index.ts
@@ -0,0 +1,6 @@
+export * from "./footer/footer.component";
+export * from "./header/header.component";
+export * from "./scroll-to-top/scroll-to-top.component";
+export * from "./about/about.component";
+export * from "./timeline/timeline.component";
+export * from "./resources/resources.component";
