fc7f21ca60038a6b9eb2cca43b6917a0e1946ea8
imsorx <srbhkp@gmail.com>
2021-08-14 20:46:26 +0530

    removed animation from body content and major style changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer2, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements AfterViewInit {
 
   isOpen: boolean = false;
+  @ViewChildren('close') links?: ElementRef[];
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    this.links?.forEach(link => {
+      this.renderer.listen(link.nativeElement, 'click', () => this.isOpen = false);
+    });
   }
-
 }
--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -9,6 +9,7 @@ export class ThemeService {
 
   activeTheme: number = 0;
   private themes: Theme[] = [
+    { primary: '#434343', secondary: '#000000' },
     { primary: '#2feaa8', secondary: '#028cf3' },
     { primary: '#40c9ff', secondary: '#e81cff' },
     { primary: '#0061ff', secondary: '#60efff' },
@@ -16,7 +17,6 @@ export class ThemeService {
     { primary: '#696eff', secondary: '#f8acff' },
     { primary: '#8711c1', secondary: '#2472fc' },
     { primary: '#8338e3', secondary: '#e0a9bb' },
-    { primary: '#858e96', secondary: '#60696b' },
     { primary: '#f9c823', secondary: '#fc506e' },
   ]
 
