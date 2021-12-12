3305e05140e231f59d27533eb044cb56115a92fd
imsorx <srbhkp@gmail.com>
2021-12-12 15:19:48 +0530

    Added animation to header menud

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,25 @@
+import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
 import { AfterViewInit, Component, ElementRef, Renderer2, ViewChildren } from '@angular/core';
 import { ThemeService } from 'src/app/services/theme.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [trigger('reveal', [
+    transition('hidden => open', [
+      query('a,button', [
+        style({ "opacity": "0", "transform": "translateX(100%)" }),
+        stagger(100, animate('250ms cubic-bezier(0.77, 0.2, 0.05, 1)', style({ "opacity": 1, "transform": "translateX(0)" })))
+      ])
+    ]),
+    transition('open => hidden', [
+      query('a,button', [
+        style({ "opacity": "1" }),
+        animate('200ms cubic-bezier(0.77, 0.2, 0.05, 1)', style({ "opacity": 0 }))
+      ])
+    ])
+  ])]
 })
 export class HeaderComponent implements AfterViewInit {
 
@@ -13,7 +28,7 @@ export class HeaderComponent implements AfterViewInit {
 
   @ViewChildren('close') links?: ElementRef[];
 
-  constructor(private renderer: Renderer2, private theme: ThemeService) { 
+  constructor(private renderer: Renderer2, private theme: ThemeService) {
     let darkModeOn = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
     darkModeOn && this.toggle();
   }
@@ -25,7 +40,7 @@ export class HeaderComponent implements AfterViewInit {
   }
 
   toggle() {
-    this.isDarkTheme = ! this.isDarkTheme;
+    this.isDarkTheme = !this.isDarkTheme;
     this.theme.changeTheme();
   }
 }
