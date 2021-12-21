ade02a04167bb036680765a63829ecd9f2fa3e91
imsorx <srbhkp@gmail.com>
2021-12-22 00:05:57 +0530

    Upgraded to angular 13 & changes in about component for trimming readme

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
@@ -10,10 +11,15 @@ export class AboutComponent implements OnInit {
 
   url = environment.aboutURL;
   isFailed: boolean = false;
+  content: string | undefined;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.http.get<string>(this.url,{responseType: 'text' as 'json'}).subscribe(res => {
+      this.content = res.split('\n').splice(0,38).join('\n');
+    });
+  }
 
   onError(event: string) {
     this.isFailed = true;
